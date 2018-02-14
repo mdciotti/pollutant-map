ebd79bca980ef7c34ad92ea0dd7e106695adc15f
Maxwell Ciotti <max@mdc.io>
2018-02-14 12:49:11 -0600

    Add mpl cmaps

---
+++ src/cmaps/index.js
@@ -0,0 +1,6 @@
+import magma from './magma.js';
+import inferno from './inferno.js';
+import plasma from './plasma.js';
+import viridis from './viridis.js';
+
+export default { magma, inferno, plasma, viridis };
